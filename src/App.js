471acc84e37e84fe47321615baed79bc5912090f
Abhishek500Kumar <103578227+Abhishek500Kumar@users.noreply.github.com>
2022-07-05 18:25:05 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { incNumber } from "./actions/index";
 import { decNumber } from "./actions/index";
-console.log('now all things run ok')
+console.log('hello all')
 import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
