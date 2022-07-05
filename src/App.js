ee2e4deb6f53428a799f6aed7d54059c8d18c007
Abhishek500Kumar <103578227+Abhishek500Kumar@users.noreply.github.com>
2022-07-05 18:05:30 +0530

    save new changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { incNumber } from "./actions/index";
 import { decNumber } from "./actions/index";
-console.log('hit now')
+console.log('now all things run ok')
 import { useSelector, useDispatch } from "react-redux";
 
 const App = () => {
